3331c1448b64cb583a7b5a2bae3eaffc1eeeb8ba
minhan1410 <79202343+minhan1410@users.noreply.github.com>
2021-05-20 16:50:55 +0700

    Lộ trình JavaWeb

--- JavaCore/LT/Buoi2/Main.java
+++ JavaCore/LT/Buoi2/Main.java
@@ -1,12 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         Math math = new Math();
-        System.out.println(math.isOdd(4));
-        System.out.println(math.calculateBMI(1.68f, 60f));
-        System.out.println(math.isTriangle(12, 10, 5));
-        System.out.println(math.smallestIn3Numbers(3,4,300));
+        System.out.println("Isodd= "+math.isOdd(4));
+        System.out.println("calculateBMI= "+math.calculateBMI(1.68f, 60f));
+        System.out.println("isTriangle= "+math.isTriangle(12, 10, 5));
+        System.out.println("smallestIn3Numbers= "+math.smallestIn3Numbers(3,300,300));
 
         int a[]=new int[]{1,2,3,4,5,6,7,8};
-        System.out.println("Sum= "+math.sum(a)+" Trung binh cong= "+math.average(a));
+        System.out.println("Sum= "+math.sum(a)+" - Trung binh cong= "+math.average(a));
+
+        System.out.print("reverseArray:");
+        a=math.reverseArray(a);
+        for(int i=0;i<a.length; i++){
+            System.out.printf("%d, ",a[i]);
+        }
+
+        System.out.println("\ncountOddNumber: "+math.countOddNumber(a));
+
+        System.out.print("oddNumberArray:");
+        int odd[]=math.oddNumberArray(a);
+        for(int i=0;i<odd.length; i++){
+                System.out.printf("%d, ",odd[i]);
+        }
+
+        System.out.println("\nbmiRating: "+math.bmiRating(1.68f,60));
     }
 }
--- JavaCore/LT/Buoi2/Math.java
+++ JavaCore/LT/Buoi2/Math.java
@@ -55,7 +55,53 @@ public class Math {
     }
 
     public int[] reverseArray(int[] numbers) {
-        return null;
+        int n[] = new int[numbers.length];
+        for (int i = 0; i < n.length; i++) {
+            n[i] = numbers[numbers.length - 1 - i];
+        }
+        return n;
     }
 
+    // Đếm số lẻ trong một mảng
+    public int countOddNumber(int[] numbers) {
+        int dem=0;
+        for(int i=0; i<numbers.length; i++){
+            if(isOdd(numbers[i])){
+                dem++;
+            }
+        }
+        return dem;
+    }
+
+    // Trả về mảng chỉ gồm các số lẻ. Ví dụ đầu vào 1, 2, 4, 5, 3 thì trả về [1, 5,
+    // 3]
+    public int[] oddNumberArray(int[] numbers) {
+        int n[]=new int[countOddNumber(numbers)];
+        int vt=0;
+        for(int i=0; i<numbers.length; i++){
+            if(isOdd(numbers[i])){
+                n[vt++]=numbers[i];
+            }
+        }
+        return n;
+    }
+
+    /*
+     * Dựa vào chỉ số BMI hãy trả về các đánh giá: Nhẹ cân, Bình thường, Quá cân,
+     * Rất nguy hiểm BMI Đánh giá Dưới 18.5 Nhẹ cân 18.5—24.9 Bình thường 25.0—29.9
+     * Quá cân 30.0 Rất nguy hiểm
+     */
+    public String bmiRating(float height, float weight) {
+        float bmi=calculateBMI(height, weight);
+        if(bmi<18.5){
+            return "Nhẹ cân";
+        }else if(bmi>=18.5 && bmi<=24.9){
+            return "Bình thường";
+        }else if(bmi>=25.0 && bmi<=29.9){
+            return "Quá cân";
+        }else if(bmi>=30){
+            return "Rất nguy hiểm";
+        }
+        return " ";
+    }
 }
