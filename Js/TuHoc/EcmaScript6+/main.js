567535eccc9a0e9a847f39f6ec2bfc8965765c8f
minhan1410 <79202343+minhan1410@users.noreply.github.com>
2021-12-11 15:04:00 +0700

    ES6

--- Js/TuHoc/EcmaScript6+/main.js
+++ Js/TuHoc/EcmaScript6+/main.js
@@ -1,5 +1,55 @@
-let fullName = ["Nguyễn", "Minh", "An"];
+/* let fullName = ["Nguyễn", "Minh", "An"];
 
 let [surname, middleName, name] = fullName;
 
-console.log(`${surname} ${middleName} ${name}`);
+console.log(`${surname} ${middleName} ${name}`); */
+
+// ----------------------------------------------------------------
+
+/* function Car(name, color) {
+    this.name = name;
+    this.color = color;
+
+    this.getName = () => {
+        return this.name;
+    };
+}
+
+const mazda = new Car("mazda6", "black");
+console.log(mazda, " - ", mazda.getName());
+
+const bmw = new Car("bmw", "white");
+console.log(bmw, " - ", bmw.getName()); */
+
+/* class Car1 {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+const mazda1 = new Car1("mazda6", "black");
+console.log(mazda1, " - ", mazda1.getName());
+
+const bmw1 = new Car1("bmw", "white");
+console.log(bmw1, " - ", bmw1.getName()); */
+
+// ----------------------------------------------------------------
+
+/* const sum = (...arr) => arr.reduce((a, b) => a + b);
+
+console.log(sum(1, 2, 3, 4, 5)); */
+
+// ----------------------------------------------------------------
+
+/* const message = (type = "log", data) => {
+    console[type](data);
+};
+
+message(undefined, "An đã ở đây");
+message("error", "An đã ở đây"); */
+
