52471bd0697e978ca88724b4fef85bc7ee51da4c
minhan1410 <79202343+minhan1410@users.noreply.github.com>
2022-02-12 14:02:24 +0700

    update

---
+++ SpringBoot/TaiLieu/Markdown/Lesson7_@ComponentScan/demo/src/main/java/com/example/demo/Girl.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Girl {
+    @Override
+    public String toString() {
+        return "Girl.java";
+    }
+}
